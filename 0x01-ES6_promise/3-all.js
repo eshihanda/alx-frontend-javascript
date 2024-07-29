d86ab3b0a0e9839565e05ebd90a46251a6c4ef18
eshihanda <eshihandamonn@gmail.com>
2024-07-29 08:14:08 +0000

    reject promoses

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-  Promise.all(promises).then((resolve) => {
-    console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
-  });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }

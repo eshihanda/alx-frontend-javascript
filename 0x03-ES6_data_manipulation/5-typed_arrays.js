73747b042999b6eb6f1a42477b71f149463ffe25
eshihanda <eshihandamonn@gmail.com>
2024-07-30 21:11:39 +0000

    all others

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createIntTypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return view;
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,3 @@
+export default function setFromArray(argument) {
+  return new Set(argument);
+}
